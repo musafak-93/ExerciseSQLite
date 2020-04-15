9d171d0eeb84e763ceaca07f4ec6b9a09c5962db
Musafak <musafak.ft18@mail.umy.ac.id>
2020-04-15 16:56:25 +0700

    mendeklarasikan edit text yang akan disimpan di tambah data

--- app/src/main/java/com/example/exercisesqlite/tambah_data.java
+++ app/src/main/java/com/example/exercisesqlite/tambah_data.java
@@ -3,12 +3,22 @@ package com.example.exercisesqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class tambah_data extends AppCompatActivity {
+
+    int id_To_Update = 0;
+    private DBHelper mydb;
+    EditText etNama, etNohp, etEmail, etAlamat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_data);
+
+        etNama = (EditText) findViewById(R.id.Et_Nama);
+        etNohp = (EditText) findViewById(R.id.Et_Nohp);
+        etEmail = (EditText) findViewById(R.id.Et_Email);
+        etAlamat = (EditText) findViewById(R.id.Et_Alamat);
     }
 }
