ee41998d92fd4c938076ba878d343716734e1a92
Musafak <musafak.ft18@mail.umy.ac.id>
2020-04-15 15:59:31 +0700

    Create table database Daftar kontak

--- app/src/main/java/com/example/exercisesqlite/DBHelper.java
+++ app/src/main/java/com/example/exercisesqlite/DBHelper.java
@@ -1,4 +1,17 @@
 package com.example.exercisesqlite;
 
-public class DBHelper {
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.HashMap;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "MyDBName.db";
+    public static final String TABLE_NAME = "DaftarKontak";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAMA = "nama";
+    public static final String COLUMN_PHONE = "phone";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_ALAMAT = "alamat";
+
+    private HashMap hp;
 }
