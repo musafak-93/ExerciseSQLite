428c7cefb4edf4605ef27d50cbc16d8327fa1f3e
Musafak <musafak.ft18@mail.umy.ac.id>
2020-04-15 17:41:43 +0700

    add motode onkeydown

--- app/src/main/java/com/example/exercisesqlite/MainActivity.java
+++ app/src/main/java/com/example/exercisesqlite/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.security.Key;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -55,6 +57,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public boolean onKeyDown(int keycode, KeyEvent event) {
+        if (keycode == KeyEvent.KEYCODE_BACK){
+            moveTaskToBack(true);
+        }
+        return super.onKeyDown(keycode, event);
     }
 
     public void pindah() {
