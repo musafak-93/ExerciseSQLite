cd8d09bff8787bd14a7f4988129292df2a430b3d
Musafak <musafak.ft18@mail.umy.ac.id>
2020-04-15 16:08:03 +0700

    Create methode onCreate

--- app/src/main/java/com/example/exercisesqlite/DBHelper.java
+++ app/src/main/java/com/example/exercisesqlite/DBHelper.java
@@ -1,5 +1,7 @@
 package com.example.exercisesqlite;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.HashMap;
@@ -14,4 +16,15 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String COLUMN_ALAMAT = "alamat";
 
     private HashMap hp;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //TODO Auto-generated method stub
+        db.execSQL("create table DaftarKontak" +
+                "(id integer primary key, nama text, phone text, email text, alamat text)");
+    }
 }
