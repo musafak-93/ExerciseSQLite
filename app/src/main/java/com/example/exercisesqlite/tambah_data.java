17ca3024761aef7169ac904de81dec966ec30b5c
Musafak <musafak.ft18@mail.umy.ac.id>
2020-04-15 17:13:19 +0700

    add function untuk save data yang diambil dari edit text

--- app/src/main/java/com/example/exercisesqlite/tambah_data.java
+++ app/src/main/java/com/example/exercisesqlite/tambah_data.java
@@ -2,7 +2,10 @@ package com.example.exercisesqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class tambah_data extends AppCompatActivity {
@@ -20,5 +23,45 @@ public class tambah_data extends AppCompatActivity {
         etNohp = (EditText) findViewById(R.id.Et_Nohp);
         etEmail = (EditText) findViewById(R.id.Et_Email);
         etAlamat = (EditText) findViewById(R.id.Et_Alamat);
+
+        mydb = new DBHelper(this);
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            int Value = extras.getInt("id");
+
+            if (Value>0){
+                Cursor rs = mydb.getData(Value);
+                id_To_Update = Value;
+                rs.moveToFirst();
+
+                String name = rs.getString(rs.getColumnIndex(DBHelper.COLUMN_NAMA));
+                String phone = rs.getString(rs.getColumnIndex(DBHelper.COLUMN_PHONE));
+                String email = rs.getString(rs.getColumnIndex(DBHelper.COLUMN_EMAIL));
+                String alamat = rs.getString(rs.getColumnIndex(DBHelper.COLUMN_ALAMAT));
+
+                if (!rs.isClosed()) {
+                    rs.close();
+                }
+
+                Button b = (Button) findViewById(R.id.Btn_Save);
+                b.setVisibility(View.INVISIBLE);
+
+                etNama.setText((CharSequence)name);
+                etNama.setFocusable(false);
+                etNama.setClickable(false);
+
+                etNohp.setText((CharSequence)phone);
+                etNohp.setFocusable(false);
+                etNohp.setClickable(false);
+
+                etEmail.setText((CharSequence)email);
+                etEmail.setFocusable(false);
+                etEmail.setClickable(false);
+
+                etAlamat.setText((CharSequence)alamat);
+                etAlamat.setFocusable(false);
+                etAlamat.setClickable(false);
+            }
+        }
     }
 }
