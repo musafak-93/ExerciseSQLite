f655c813b2002085d0a8054fea21f5ad0d5f3799
Musafak <musafak.ft18@mail.umy.ac.id>
2020-04-16 20:28:41 +0700

    Add function untuk menampilkan menu pop up edit sama delete

--- app/src/main/java/com/example/exercisesqlite/MainActivity.java
+++ app/src/main/java/com/example/exercisesqlite/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.exercisesqlite;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -13,6 +16,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -20,7 +24,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.security.Key;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     public final static String EXTRA_MESSAGE = "MESSAGE";
     private ListView listView;
@@ -33,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         klik = findViewById(R.id.fab);
-
         klik.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,11 +46,14 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
+        //Database SQLite
         mydb = new DBHelper(this);
         ArrayList arrayList = mydb.getAllContacts();
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
 
         listView = (ListView) findViewById(R.id.LV);
+        registerForContextMenu(listView);
         listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -62,6 +68,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+   public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getMenuInflater().inflate(R.menu.popup, menu);
     }
 
     public boolean onKeyDown(int keycode, KeyEvent event) {
